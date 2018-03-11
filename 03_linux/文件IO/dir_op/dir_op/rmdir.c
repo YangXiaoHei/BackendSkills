7a583ce34406b8434b910f8325e7f855959c5856
yangxiaohei <yangxiaohei321123@163.com>
2018-03-11 18:53:53 +0800

    部分 API 函数

--- 03_linux/文件IO/dir_op/dir_op/rmdir.c
+++ 03_linux/文件IO/dir_op/dir_op/rmdir.c
@@ -7,3 +7,20 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    
+    if (argc < 2) {
+        printf("至少需要 1 个命令行参数\n");
+        exit(1);
+    }
+    
+    if (rmdir(argv[1]) < 0) {
+        perror(argv[1]);
+        exit(1);
+    }
+    
+    return 0;
+}
