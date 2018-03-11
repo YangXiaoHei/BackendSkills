39630a7b4a096e2e59513eadbbb7f8b406571d0e
yangxiaohei <yangxiaohei321123@163.com>
2018-03-11 18:42:52 +0800

    部分 API 函数

--- 03_linux/文件IO/dir_op/dir_op/mkdir.c
+++ 03_linux/文件IO/dir_op/dir_op/mkdir.c
@@ -7,3 +7,24 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[]) {
+    
+    if (argc < 2) {
+        printf("命令行至少需要 1 个参数\n");
+        exit(1);
+    }
+    
+    umask(0);
+    
+    if (mkdir(argv[1], 0777) < 0) {
+        perror(argv[1]);
+        exit(1);
+    }
+    
+    return 0;
+}
--- 03_linux/文件IO/dir_op/dir_op/opendir.c
+++
@@ -1,9 +0,0 @@
-//
-//  opendir.c
-//  dir_op
-//
-//  Created by YangHan on 2018/3/11.
-//  Copyright © 2018年 YangHan. All rights reserved.
-//
-
-#include <stdio.h>
