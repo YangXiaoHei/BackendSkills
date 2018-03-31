bd54aff6140a48f2815fb77694275eeb568c41a0
yangxiaohei <yangxiaohei321123@163.com>
2018-04-01 03:15:18 +0800

    update

--- 03_linux/Libevent-Study5/Libevent-Study5/main.c
+++ 03_linux/Libevent-Study5/Libevent-Study5/main.c
@@ -11,10 +11,22 @@
 #include "DList.h"
 #include <time.h>
 
-int main(int argc, const char * argv[]) {
-    
+void test1() {
     clock_t s = clock();
+    
+    SList sl = sl_create();
+    for (int i = 0; i < 2000000; i++) {
+        sl_insert_tail(sl, i);
+    }
+    sl_remove_all(sl);
+    
+    clock_t e = clock();
+    printf("总时间 : %.3f\n", (e - s) * 1.0 / CLOCKS_PER_SEC);
+}
 
+void test2() {
+    clock_t s = clock();
+    
     DList dl = dl_create();
     for (int i = 0; i < 2000000; i++) {
         dl_insert_tail(dl, i);
@@ -23,16 +35,10 @@ int main(int argc, const char * argv[]) {
     
     clock_t e = clock();
     printf("总时间 : %.3f\n", (e - s) * 1.0 / CLOCKS_PER_SEC);
-    
-    s = clock();
-    SList sl = sl_create();
-    for (int i = 0; i < 2000000; i++) {
-        sl_insert_tail(sl, i);
-    }
-    sl_remove_all(sl);
-    
-    e = clock();
-    printf("总时间 : %.3f\n", (e - s) * 1.0 / CLOCKS_PER_SEC);
-    
+}
+
+int main(int argc, const char * argv[]) {
+//    test1();
+    test2();
     return 0;
 }
