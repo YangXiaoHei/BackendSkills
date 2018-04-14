9a8deb07dcf067f0a57267c2e00201123f658790
yangxiaohei <yangxiaohei321123@163.com>
2018-04-14 23:04:12 +0800

    API熟悉

--- Unix_Network_Programming_Volume_1/Chapter_01/hostbyteorder/hostbyteorder/main.c
+++ Unix_Network_Programming_Volume_1/Chapter_01/hostbyteorder/hostbyteorder/main.c
@@ -8,6 +8,8 @@
 
 #include <stdio.h>
 
+#include <arpa/inet.h>
+
 /**
  *  内存模型图测试
  */
@@ -34,9 +36,58 @@ void test() {
     }
 }
 
+/**
+ *  转换为网络字节序，大端，如果在大端机器中，hton 等宏被定义为空宏
+ */
+void test1() {
+    short a = 0x1234;
+    printf("%x\n", htons(a));
+}
+
+/**
+ *  inet_aton、inet_addr、inet_ntoa
+    ⚠️ 以下函数均被废弃，不要再使用
+ */
+void test2() {
+    
+    // inet_aton
+    {
+        struct sockaddr_in addr;
+        // 成功返回 1，失败返回 0
+        if (inet_aton("10.10.3.24", &addr.sin_addr) > 0) {
+            printf("ip 地址有效 : %d\n", addr.sin_addr.s_addr);
+        } else {
+            printf("ip 地址无效");
+        }
+    }
+    
+    // inet_addr
+    {
+        int ipv4 = 0;
+        if ((ipv4 = inet_addr("10.10.3.24")) != INADDR_NONE) {
+            printf("字符串有效，返回 32 位二进制网络字节序 IPv4 地址 : %d\n", ipv4);
+        } else {
+            printf("字符串无效\n");
+        }
+    }
+   
+    // inet_ntoa
+    {
+        char *ipstr = NULL;
+        struct in_addr iaddr;
+        iaddr.s_addr = 402852362;
+        if ((ipstr = inet_ntoa(iaddr)) != NULL) {
+            printf("ip 串为 : %s\n", ipstr);
+        } else {
+            printf("ip 地址无\n");
+        }
+    }
+}
+
+
 int main(int argc, const char * argv[]) {
     
-   
+    test2();
     
     return 0;
 }
