38812e0bebd579fde5b0961525671eefa5492534
yangxiaohei <yangxiaohei321123@163.com>
2018-05-04 10:23:26 +0800

    Backend_Skills
    1，重新定位下这个仓库吧....用来记录一些后端的技能，比如 shell 脚本，linux 命令

--- linux/File_IO/practises/redirect.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main () {
-    
-    printf("redirect string\n");
-    
-    return 0;
-    
-}
--- linux/Signal/note/segf.c
+++
@@ -1,7 +0,0 @@
-int main() {
-    
-    char *c = "yanghan";
-    *c = 'H';
-    
-    return 0;
-}
